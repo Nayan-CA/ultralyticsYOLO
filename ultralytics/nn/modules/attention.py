c77be5e38a58ceda734b2d00d98e671a41d09817
Nayan-CA <97055057+Nayan-CA@users.noreply.github.com>
2026-02-21 11:52:29 +0530

    Create attention.py
    Signed-off-by: Nayan-CA <97055057+Nayan-CA@users.noreply.github.com>

---
+++ ultralytics/nn/modules/attention.py
@@ -0,0 +1,28 @@
+class ECA(nn.Module):
+    def __init__(self, channels, k_size=3):
+        super().__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.conv = nn.Conv1d(1, 1, kernel_size=k_size, 
+                              padding=k_size//2, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        y = self.avg_pool(x)
+        y = self.conv(y.squeeze(-1).transpose(-1,-2))
+        y = y.transpose(-1,-2).unsqueeze(-1)
+        return x * self.sigmoid(y)
+
+
+class SimAM(nn.Module):
+    def __init__(self, e_lambda=1e-4):
+        super().__init__()
+        self.e_lambda = e_lambda
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        b, c, h, w = x.shape
+        n = h * w - 1
+        mean = x.mean(dim=[2,3], keepdim=True)
+        var = ((x - mean) ** 2).sum(dim=[2,3], keepdim=True) / n
+        e_inv = (x - mean) ** 2 / (4 * (var + self.e_lambda)) + 0.5
+        return x * self.sigmoid(e_inv)
